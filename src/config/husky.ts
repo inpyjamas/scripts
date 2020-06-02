147262ffd88eaade9cbebf78414e9e66fdb7f99f
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-06-02 09:04:48 +0200

    dont watch in husky hook

--- src/config/husky.ts
+++ src/config/husky.ts
@@ -1,6 +1,6 @@
 const commands = [
   "npm run build",
-  "cross-env NODE_ENV=test npm run test:ci",
+  "cross-env NODE_ENV=test npm run test:ci -- --coverage=false",
   "lint-staged",
 ];
 module.exports = {
