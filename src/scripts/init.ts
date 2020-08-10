ba4755ac50435784613dcb99e752764c675b18dd
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-08-10 08:26:03 +0200

    add init script definition

---
+++ src/scripts/init.ts
@@ -0,0 +1,8 @@
+// should ask for
+// adding scripts for
+// - test
+// - dev
+// should explain what it is doing
+// should warn and expect confirmation if scripts already exsist
+// should be extendable so adding new scripts is easy
+// could offer backup of script to [SCRIPT KEY]:bak
