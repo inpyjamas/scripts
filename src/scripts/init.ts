74a24f3a7530b9416bcea81858834882f650173a
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-03-12 06:11:59 +0100

    add init script

---
+++ src/scripts/init.ts
@@ -0,0 +1,14 @@
+export async function init(): Promise<void> {
+  // TODO: ask user for input
+  // - eslintrc.js
+  // - prettier.config.js
+  // - nodemon.json
+  // - add scripts to package.json (if exists)
+  // dev
+  // test
+  // start
+}
+
+if (require.main === module) {
+  init().catch(console.error);
+}
